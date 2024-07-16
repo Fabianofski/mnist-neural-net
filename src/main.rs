32c5f54c517c0fe759313a2040d48953464b69bd
Fabian Friedrich <friedrich.fabian@gmx.net>
2024-07-16 21:57:20 +0200

    Added test and rewrote StringRecord Parser

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use csv::StringRecord; 
+use csv::StringRecord;
 
 fn save_as_image(label: u8, record: Vec<u8>) -> Result<(), Box<dyn std::error::Error>> {
     let mut img = image::ImageBuffer::new(28, 28);
-    
+
     for (i, &pixel) in record.iter().enumerate() {
         let x = (i) % 28;
         let y = (i) / 28;
@@ -17,21 +17,11 @@ fn save_as_image(label: u8, record: Vec<u8>) -> Result<(), Box<dyn std::error::E
 
 fn record_to_data(record: StringRecord) -> (u8, Vec<u8>) {
     let label = record[0].parse::<u8>().unwrap();
-    let mut pixels: Vec<u8> = Vec::new(); 
-
-    for (i, pixel) in record.iter().enumerate() {
-        if i == 0 {
-            continue;
-        }
-
-        let pixel = pixel.parse::<u8>().unwrap();
-        pixels.push(pixel);
-    }
-
+    let pixels = record.iter().skip(1).map(|x| x.parse::<u8>().unwrap()).collect();
     (label, pixels)
 }
 
-fn main(){
+fn main() {
     let mut rdr = csv::Reader::from_path("src/mnist_test.csv").unwrap();
     let record = rdr.records().next().unwrap();
 
@@ -39,4 +29,20 @@ fn main(){
     let (label, pixels) = record_to_data(record);
 
     save_as_image(label, pixels).unwrap();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_record_to_data() {
+        let vec = vec!["3", "5", "2", "6", "8", "10"];
+        let record: StringRecord = StringRecord::from(vec);
+
+        let (label, pixels) = record_to_data(record);
+
+        assert_eq!(label, 3);
+        assert_eq!(pixels, vec![5, 2, 6, 8, 10]);
+    }
 }
